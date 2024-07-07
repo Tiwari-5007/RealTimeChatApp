a44340c3a7cc61bf74908cdd07c3c874aed39290
Yash <tiwariyash222@gmail.com>
2024-07-07 21:47:41 +0530

    CORS error remove

--- frontend/socket_client.js
+++ frontend/socket_client.js
@@ -1,1 +1,17 @@
-console.log("This is working");
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialization
+    const socket = io("http://localhost:8000");
+    socket.on('connect', () => {
+        console.log('Connected to server');
+    });
+
+    // Listen for a custom event from the server
+    socket.on('message', (data) => {
+        console.log('Received message from server:', data);
+    });
+
+    // Emit a custom event to the server
+    socket.emit('message', 'Hello from client');
+    console.log("Socket: ",socket);
+
+});
